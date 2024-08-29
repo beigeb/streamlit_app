3ffd917f8c6a32ea4e05263a59cbbf21bc7b7924
YK <147385684+beigeb@users.noreply.github.com>
2024-08-30 01:43:14 +0530

    Update model_scoring_app.py

--- model_scoring_app.py
+++ model_scoring_app.py
@@ -7,7 +7,7 @@ from lime.lime_tabular import LimeTabularExplainer
 import openai
 
 # OpenAI API key
-openai.api_key = "sk-McJlAlQ-KIgV3FnxSOAJFJyAg-twiXGaOnzWY3z98rT3BlbkFJ9GDS7vyU6SlS2ixI_qpCH3Moa-LkBQzDGYCf9zdN8A"  # Replace with your actual OpenAI API key
+openai.api_key = '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' # Replace with your actual OpenAI API key
 
 # Set the page layout
 st.set_page_config(layout="wide")
